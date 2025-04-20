30e6f73237a9d32d6bb6c28c58b1f69a5bb5686d
mansijain1618 <154000709+mansijain1618@users.noreply.github.com>
2025-04-20 16:35:03 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function startSlideshow() {
+  document.querySelector('.main-popup').classList.add('hidden');
+  document.getElementById('slideshow').classList.remove('hidden');
+}
+
+function showStory() {
+  document.getElementById('slideshow').classList.add('hidden');
+  document.getElementById('ourStory').classList.remove('hidden');
+}
